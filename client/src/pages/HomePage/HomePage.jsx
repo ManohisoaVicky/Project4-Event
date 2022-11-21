0d0cc4fcf16ecdd947bfe91eec48ec0ccb2e897e
Manohisoa <manohisoarajaonarivony@gmail.com>
2022-11-21 22:57:22 +0300

    added useDebounce hook and used it in homepage

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { getEvents } from "../../utils/eventService"
+import useDebounce from '../../hooks/useDebounce'
 
 import Input from '../../components/FormElements/Input/Input'
 import EventCard from '../../components/EventCard/EventCard'
@@ -9,20 +10,16 @@ function HomePage() {
   const [events, setEvents] = useState()
   const [search, setSearch] = useState("")
 
+  const debounce = useDebounce(search, 500)
+
   useEffect(() => {
     async function getAllEvents() {
       const events = await getEvents(search)
       setEvents(events)
     }
     getAllEvents();
-  }, [search])
-
-  // const handleChange = (e) => {
-  //   setSearch((oldState) => ({
-  //     ...oldState,
-  //     [e.target.name]: e.target.value
-  //   }));
-  // }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debounce])
 
   const handleChange = (e) => {
     const value = e.target.value
